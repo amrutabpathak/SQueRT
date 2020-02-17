a96a391135776d7ba5d7a8a5cf5b13db289529d5
kaschu <schultz.kat@husky.neu.edu>
2020-02-17 15:48:41 -0800

    Mostly works now on any number of input files

--- arxiv_pdf_scraper.py
+++ arxiv_pdf_scraper.py
@@ -1,3 +1,8 @@
+'''
+To do:
+- Fix returning an extra file when input num isn't a multiple of 200
+- Exception handling if a PDF is taking too long to download
+'''
 import sys
 from bs4 import BeautifulSoup
 import urllib
@@ -11,32 +16,39 @@ def scrape(num_pdfs):
     start_file_idx = 0
     urls = []
     export_urls = []
-    count = 0
+    num_retrieved = 0
 
-    while start_file_idx + 200 <= num_pdfs:
+    #while start_file_idx + 200 <= num_pdfs:
+    while num_retrieved < num_pdfs:
         url_to_scrape = url_prefix + str(start_file_idx)
         htmltext = urllib.request.urlopen(url_to_scrape).read()
         soup = BeautifulSoup(htmltext, "html.parser")
 
         # Extract urls for all pdfs linked to on page
         for link in soup.findAll('a', href=True):
+            if len(urls) > num_pdfs:
+                break
             _FULLURL = link.get('href')
             if _FULLURL.startswith('https://arxiv.org/pdf/'):
+                #while len(urls) < num_pdfs:
+                num_retrieved += 1
                 urls.append(_FULLURL)
-                count += 1
+                #num_retrieved += 1
 
-        start_file_idx += 200   # increment start index in url by 200 to get next page of results
+        if num_pdfs % 200 == 0:    # if num_pdfs is a multiple of 200
+            start_file_idx += 200   # increment start index in url by 200 to get next page of results
 
-    print(count)
+    print("num_retrieved: ", num_retrieved)
     # Convert url list to the arxiv export form
     for addy in urls:
         addy = addy[:8] + 'export.' + addy[8:]
         export_urls.append(addy) 
 
-    #print(export_urls)
+    print(export_urls)
     print(len(export_urls)) 
+    #print(export_urls[497])
 
-    export_urls_trunc = export_urls[:3]
+    #export_urls_trunc = export_urls[:3]
 
     for doc in export_urls:
         print('Downloading %s' % doc)
