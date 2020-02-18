e0338acefc553415e034582d1402f29095adb20e
kaschu <schultz.kat@husky.neu.edu>
2020-02-17 16:49:12 -0800

    it twerks on arbitrary number of files!

--- arxiv_pdf_scraper.py
+++ arxiv_pdf_scraper.py
@@ -1,6 +1,5 @@
 '''
 To do:
-- Fix returning an extra file when input num isn't a multiple of 200
 - Exception handling if a PDF is taking too long to download
 '''
 import sys
@@ -18,7 +17,6 @@ def scrape(num_pdfs):
     export_urls = []
     num_retrieved = 0
 
-    #while start_file_idx + 200 <= num_pdfs:
     while num_retrieved < num_pdfs:
         url_to_scrape = url_prefix + str(start_file_idx)
         htmltext = urllib.request.urlopen(url_to_scrape).read()
@@ -26,28 +24,24 @@ def scrape(num_pdfs):
 
         # Extract urls for all pdfs linked to on page
         for link in soup.findAll('a', href=True):
-            if len(urls) > num_pdfs:
+            if len(urls) >= num_pdfs:
                 break
             _FULLURL = link.get('href')
             if _FULLURL.startswith('https://arxiv.org/pdf/'):
-                #while len(urls) < num_pdfs:
                 num_retrieved += 1
                 urls.append(_FULLURL)
-                #num_retrieved += 1
 
         if num_pdfs % 200 == 0:    # if num_pdfs is a multiple of 200
             start_file_idx += 200   # increment start index in url by 200 to get next page of results
 
     print("num_retrieved: ", num_retrieved)
+
     # Convert url list to the arxiv export form
     for addy in urls:
         addy = addy[:8] + 'export.' + addy[8:]
         export_urls.append(addy) 
 
-    print(export_urls)
-    print(len(export_urls)) 
-    #print(export_urls[497])
-
+    #print(export_urls)
     #export_urls_trunc = export_urls[:3]
 
     for doc in export_urls:
