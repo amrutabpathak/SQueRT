6f623544cc6e8a688a75be25cac09656fd10e238
simpsone <simpson.ev@husky.neu.edu>
2020-04-20 15:40:17 -0700

    stage 1 hugging face transition checkpoint

--- RelevantSnippets.py
+++ RelevantSnippets.py
@@ -9,6 +9,8 @@ import csv
 import heapq
 
 # In[ ]:
+from transformers import DistilBertTokenizer, DistilBertModel
+import torch
 
 
 class QuerySnippet:
@@ -30,26 +32,29 @@ class QuerySnippet:
 
 def returnRelevant(researchPaper, query, numSnippets = 15):
     # Make sure these are downloaded before using
-    # model_list = ['en_trf_bertbaseuncased_lg','en_trf_robertabase_lg','en_trf_distilbertbaseuncased_lg']
-    # model_list = ['en_trf_bertbaseuncased_lg']
-    model_list = ['en_trf_distilbertbaseuncased_lg']
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+    model = DistilBertModel.from_pretrained('distilbert-base-cased')
     relevantSnippets = []
-    
-    for model in model_list:
-        researchPpr_NLP = spacy.load(model)
-        with open(researchPaper, encoding='utf8') as researchPaperCSV:
-            researchPaperReader = csv.reader(researchPaperCSV)
-            score_max_heap = []
-            queryObj = researchPpr_NLP(query)
-            for snippet in researchPaperReader:
-                if('<EOS>' not in snippet):
-                    snippetStr = " "
-                    snippetStr = ' '.join([str(elem) for elem in snippet])
-                    snippetObj = researchPpr_NLP(snippetStr)
-                    qs = QuerySnippet(query, snippet, queryObj.similarity(snippetObj))
-                    if len(score_max_heap) < numSnippets or qs.similarity > score_max_heap[0].similarity:
-                        if len(score_max_heap) == numSnippets: heapq.heappop(score_max_heap)
-                        heapq.heappush(score_max_heap, qs)
-            for qs in score_max_heap:
-                relevantSnippets.append(qs.snippet)
+
+    #researchPpr_NLP = spacy.load(model)
+    with open(researchPaper, encoding='utf8') as researchPaperCSV:
+        researchPaperReader = csv.reader(researchPaperCSV)
+        score_max_heap = []
+        input_ids = torch.tensor(tokenizer.encode(query, add_special_tokens=True, pad_to_max_length=True)).unsqueeze(0)
+        queryObj = model(input_ids)[0].squeeze()
+        #queryObj = model(query)
+        for snippet in researchPaperReader:
+            if('<EOS>' not in snippet):
+                snippetStr = " "
+                snippetStr = ' '.join([str(elem) for elem in snippet])
+                input_ids = torch.tensor(tokenizer.encode(snippetStr, add_special_tokens=True, pad_to_max_length=True)).unsqueeze(0)
+                snippetObj = model(input_ids)[0].squeeze()
+                #snippetObj = model(snippetStr)
+                #qs = QuerySnippet(query, snippet, queryObj.similarity(snippetObj))
+                qs = QuerySnippet(query, snippet, torch.cosine_similarity(queryObj, snippetObj))
+                if len(score_max_heap) < numSnippets or qs.similarity > score_max_heap[0].similarity:
+                    if len(score_max_heap) == numSnippets: heapq.heappop(score_max_heap)
+                    heapq.heappush(score_max_heap, qs)
+        for qs in score_max_heap:
+            relevantSnippets.append(qs.snippet)
     return relevantSnippets
